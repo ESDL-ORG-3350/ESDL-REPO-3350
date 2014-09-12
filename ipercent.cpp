7cfe51f9774175df13ee3470fa37116cb8ff09c6
kriti-mandloi <mandloikriti@cumminscollege.in>
2014-09-12 09:23:36 +0530

    added!!!
    No Description

--- ipercent.cpp
+++ ipercent.cpp
@@ -6,5 +6,6 @@ int main()
 cout <<"This is the repository created by owner of the orgasnisation ";
 cout<<"we made the changes in owner file";
 cout <<"We edited file after fork";
+cout<<"Notepad Edited";
 return 0;
 }
